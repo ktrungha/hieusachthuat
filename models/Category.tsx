a5194985d5cb7831f36ba643d07a53f77be0ba77
Trung Ha <ktrungha@yahoo.com>
2019-03-24 10:57:53 +0700

    add new categories

--- models/Category.tsx
+++ models/Category.tsx
@@ -2,6 +2,8 @@ export default {
   sachGiaoTrinh: 'Sách Giáo Trình',
   sachThieuNhi: 'Sách Thiếu Nhi',
   sachLuyenThi: 'Sách Luyện Thi',
+  sachThamKhao: 'Sách Tham Khảo',
+  sachDoiSong: 'Sách Đời Sống',
   truyen: 'Truyện',
   tudien: 'Từ điển',
 } as { [key: string]: string };
